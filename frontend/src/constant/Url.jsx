3515369883b11de70956fd0af57c696e16458d45
sadhanapatel1998 <sadhanapatel1998@gmail.com>
2026-08-04 17:32:49 +0530

    update API_URL and email configurations for production

--- frontend/src/constant/Url.jsx
+++ frontend/src/constant/Url.jsx
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://localhost:5000'; //local ke liye
-export const API_URL = "https://www.api.interradecors.com"; //LIVE ke liye
+export const API_URL = "https://api.interradecors.com"; //LIVE ke liye
